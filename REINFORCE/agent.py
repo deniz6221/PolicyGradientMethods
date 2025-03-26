48fea7d755ac3e44349f0b6e87d527d3d55fcb32
Deniz <deniz.poyraz@std.bogazici.edu.tr>
2025-03-26 18:38:42 +0300

    Hyperparams updated

--- REINFORCE/agent.py
+++ REINFORCE/agent.py
@@ -1,12 +1,13 @@
 import torch
 from torch import optim
+import torch.utils
 
 from model import VPG
 import torch.nn.functional as F
 from torch.distributions import Normal
 
 gamma = 0.99
-learning_rate = 0.01
+learning_rate = 5e-6
 
 class Agent():
     def __init__(self):
@@ -16,14 +17,14 @@ class Agent():
         self.rewards = []
         self.log_probs = []
         
-    def decide_action(self, state):
+    def decide_action(self,  state):
         action_mean, act_std = self.model(state).chunk(2, dim=-1)
-        action_std = F.softplus(act_std) + 5e-2  # increase variance to stimulate exploration
+        action_std = act_std.exp() + 0.05
         dist = Normal(loc=action_mean, scale=action_std)
-        action = dist.sample()
+        action = dist.rsample()
         prob = dist.log_prob(action).sum(-1).unsqueeze(-1)
         self.log_probs.append(prob)
-        return action
+        return action.detach()
     
     def update_model(self):
         if not self.rewards:
@@ -38,16 +39,16 @@ class Agent():
             returns.insert(0, Rt)
 
         returns = torch.tensor(returns, dtype=torch.float32)
-        returns = (returns - returns.mean()) / (returns.std() + 1e-8)  
+        #returns = (returns - returns.mean()) / (returns.std() + 1e-5)  
 
-        
         for i in range(len(returns)):
             loss_lst.append(-self.log_probs[i]*returns[i])
 
         
         self.optimizer.zero_grad()
-        loss = torch.cat(loss_lst).sum()    
+        loss = torch.cat(loss_lst).mean()
         loss.backward()
+        torch.nn .utils.clip_grad_norm_(self.model.parameters(), 0.5)
         self.optimizer.step()
 
         # Clear memory
---
+++ REINFORCE/plotter.py
@@ -0,0 +1,25 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+import json
+
+file_name = "rewjson_999"
+rps_lst = json.load(open(f"checkpoints/{file_name}.json", "r"))
+
+
+rps_data = np.array(rps_lst)
+
+
+window_size = 100
+
+
+smoothed_loss = np.convolve(rps_data, np.ones(window_size)/window_size, mode='valid')
+lossX = [i for i in range(len(smoothed_loss))]
+
+plt.plot(lossX, smoothed_loss)
+plt.title("RPS over episodes")
+plt.xlabel("Episode")
+plt.ylabel("RPS")
+plt.grid()
+plt.legend()
+plt.show()
